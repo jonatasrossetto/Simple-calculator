115c13e35dcdb7554d7f79d2811133b97ac2a8fe
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2022-04-14 16:00:21 -0300

    update 14-04-2022 - Simple buttons, trying to implement add functionality

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Simple Calculator in JS
+This is a homemade project to create a simple calculator using JavaScript
+The first goal is to implement the four basic functions with integer numbers
+
+14/04/2022 - Simple buttons, trying to implement add functionality, still very basic
---
+++ script.js
@@ -0,0 +1,113 @@
+"use strict";
+let btnAdd = document.querySelector(".add");
+let btnCalculate = document.querySelector(".calculate");
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
+let btn7 = document.querySelector(".btn7");
+let btn8 = document.querySelector(".btn8");
+let btn9 = document.querySelector(".btn9");
+let btn0 = document.querySelector(".btn0");
+let visor = document.getElementById("visor");
+let visorStr = "0";
+visor.textContent = visorStr;
+let nA = 0;
+let nB = 0;
+let operation = "";
+
+btnAdd.addEventListener("click", function () {
+  console.log("btnAdd");
+  operation = "add";
+  nA = Number(visorStr);
+  visorStr = "0";
+  console.log(`number A: ${nA}`);
+  console.log(`operation: ${operation}`);
+  visor.textContent = visorStr;
+});
+
+btnCalculate.addEventListener("click", function () {
+  console.log("calculate");
+  nB = Number(visorStr);
+  visorStr = nA+nB;
+  console.log(`number A: ${nA}`);
+  console.log(`number B: ${nB}`);
+  console.log(`operation: ${operation}`);
+  console.log(`result: ${nA+nB}`)
+  visor.textContent = visorStr;
+  operation = "";
+  nA=0;
+  nB=0;
+});
+
+btn1.addEventListener("click", function () {
+  console.log("btn1");
+  visorStr = visorStr + "1";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn2.addEventListener("click", function () {
+  console.log("btn2");
+  visorStr = visorStr + "2";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn3.addEventListener("click", function () {
+  console.log("btn3");
+  visorStr = visorStr + "3";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn4.addEventListener("click", function () {
+  console.log("btn4");
+  visorStr = visorStr + "4";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn5.addEventListener("click", function () {
+  console.log("btn5");
+  visorStr = visorStr + "5";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn6.addEventListener("click", function () {
+  console.log("btn6");
+  visorStr = visorStr + "6";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn7.addEventListener("click", function () {
+  console.log("btn7");
+  visorStr = visorStr + "7";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn8.addEventListener("click", function () {
+  console.log("btn8");
+  visorStr = visorStr + "8";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn9.addEventListener("click", function () {
+  console.log("btn9");
+  visorStr = visorStr + "9";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
+
+btn0.addEventListener("click", function () {
+  console.log("btn0");
+  visorStr = visorStr + "0";
+  console.log(visorStr);
+  visor.textContent = visorStr;
+});
